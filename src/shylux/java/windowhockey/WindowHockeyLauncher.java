881fb5254f6ca377eaa7c778f3f7f9ee6ed39618
Lukas Knoepfel <shylux@gmail.com>
2013-10-26 01:33:13 +0200

    Added launcher class.

---
+++ src/shylux/java/windowhockey/WindowHockeyLauncher.java
@@ -0,0 +1,12 @@
+package shylux.java.windowhockey;
+
+public class WindowHockeyLauncher {
+
+	public static void main(String[] args) {
+		new WindowHockeyLauncher();
+	}
+	
+	public WindowHockeyLauncher() {
+
+	}
+}
